d22d434971496992122345ed53cae37874c5cd9a
AshrayaDargarh <ashraydargar01@gmail.com>
2023-10-08 16:36:59 +0530

    backend Auth

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,22 @@ const dotenv=require("dotenv");
 const cors=require('cors')
 const chats=require("./data/data");
 const connectDB = require("./config/db");
-
+const userRoutes=require('./routes/userRoutes')
+const {notFound,errorHandler}=require('./middleware/errorMiddleware')
 
 const app=express();
 dotenv.config()
 connectDB();
 app.use(cors())
-
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("API is running");
 })
 
-app.get('/api/chat',(req,res)=>{
-    console.log(chats);
-    res.send(chats);
-})
+app.use('/api/user',userRoutes);
 
-app.get('/api/chat/:id',(req,res)=>{
-    const singleChat=chats.find(c=>c._id===req.params.id);
-    console.log(singleChat);
-    res.send(singleChat);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT=process.env.PORT ||5000;
 app.listen(PORT,()=>{
